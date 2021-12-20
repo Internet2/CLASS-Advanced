bad6229e6f1e3aa16916a9465b8103afd33084dd
Timothy Middelkoop <tmiddelkoop@internet2.edu>
2021-12-20 09:27:26 -0600

    Update setup-user.sh

--- multi-cloud/terraform/scripts/setup-10-user.sh
+++ multi-cloud/terraform/scripts/setup-10-user.sh
@@ -6,26 +6,18 @@ echo "=== setup-user.sh $(hostname) $(date)"
 # install packages on deb based systems
 if [ -x /usr/bin/apt-get -a -x /usr/bin/sudo ] ; then
     sudo DEBIAN_FRONTEND=noninteractive apt-get install -y git python3 ca-certificates procps wget curl unzip jq
-    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y bash-completion rsync
-    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends emacs-nox aspell-en
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y vim bash-completion rsync
 fi
 
 # install packages on rpm based systems.
 if [ -x /usr/bin/yum ] ; then
     sudo yum install -y git python3 ca-certificates procps wget curl unzip jq
-    sudo yum install -y bash-completion rsync
-    sudo yum install -y emacs-nox hunspell-en
+    sudo yum install -y vim bash-completion rsync
 fi
 
 # directories
 install -dv ~/projects
 install -dv -m 700 ~/tmp
-
-# ssh keys
-install -v -d -m 700 ~/.ssh
-if [ ! -r ~/.ssh/authorized_keys ] ; then
-    ( umask 0077 ; ssh-add -L > ~/.ssh/authorized_keys )
-fi
 
 # bash local configuration
 if ! grep -q "^# local configuration" ~/.bashrc ; then
@@ -35,14 +27,12 @@ if ! grep -q "^# local configuration" ~/.bashrc ; then
 fi
 
 # local configuration
-echo -e "(setq make-backup-files nil)\n(menu-bar-mode -1)" > ~/.emacs
-echo "PERL_HOMEDIR=0" > ~/.perl-homedir
+install -dpv ~/.local/share/R
 echo "R_LIBS_USER=~/.local/share/R" > ~/.Renviron
+echo "PERL_HOMEDIR=0" > ~/.perl-homedir
 
 # git
 git config --global color.ui auto
 git config --global push.default simple
 git config --global pull.ff only
-if [ -x /usr/bin/emacs ] ; then
-    git config --global core.editor 'emacs -nw' # overidden by GIT_EDITOR
-fi
+git config --global init.defaultBranch main
